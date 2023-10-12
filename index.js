bcc7104706f6a36e3d73facd047647509e76d093
Noor-Alrai <ameinnoor91@gmail.com>
2023-10-12 13:50:01 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+const submitData = (userName, userEmail) => {
+  const url = "http://localhost:3000/users";
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch(url, config)
+    .then((response) => response.json())
+    .then((user) => {
+      const newId = user.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = `New ID: ${newId}`;
+      document.body.appendChild(idElement);
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      const errorElement = document.createElement("p");
+      errorElement.textContent = `Error: ${errorMessage}`;
+      document.body.appendChild(errorElement);
+    });
+};
+
+submitData("Ahmad", "a.gmail.com");
